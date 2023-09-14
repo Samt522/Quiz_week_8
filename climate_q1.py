1010ea19bc5380fdcb4970c3623cbb0d5a96726d
Samt522 <samuel.taverna@griffithuni.edu.au>
2023-09-14 11:01:45 +1000

    Connected db

--- climate_q1.py
+++ climate_q1.py
@@ -1,9 +1,29 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3 
+
+connection = sqlite3.connect('climate.db')
+cur = connection.cursor()
+
 years = []
 co2 = []
 temp = []
 
+yearInsert = (" SELECT YEAR FROM ClimateData; ")
+
+for row in cur.execute(yearInsert):
+    years.append(row)
+    
+co2Insert = (" SELECT CO2 FROM ClimateData; ")
+
+for row in cur.execute(co2Insert):
+    co2.append(row)
+    
+TempInsert = (" SELECT Temperature FROM ClimateData; ")
+
+for row in cur.execute(TempInsert):
+    temp.append(row)
+    
+  
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
 plt.title("Climate Data") 
@@ -16,3 +36,6 @@ plt.ylabel("Temp (C)")
 plt.xlabel("Year (decade)") 
 plt.show() 
 plt.savefig("co2_temp_1.png") 
+
+
+connection.close()
